8b218562e194806d83cb931e60cbe5a8b91e7504
Gaurav30413 <bussinessideas01@gmail.com>
2024-06-10 17:35:51 +0530

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector("#menu-bars");
-let navbar = document.querySelector(".navbar")
-
-menu.oneclick = () => {
-    menu.classList.toggle('fa-time');
-    navbar.classList.toggle('active');
-}
